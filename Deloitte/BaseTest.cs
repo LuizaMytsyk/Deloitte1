9706768e440e11acf5c271e4bb7c3c80f795bf14
LuizaMytsyk <luiza.mytsyk@gmail.com>
2018-10-16 14:29:20 +0300

    some changes

--- Deloitte/BaseTest.cs
+++ Deloitte/BaseTest.cs
@@ -42,8 +42,8 @@ namespace DeloitteTests
         [OneTimeTearDown]
         public void OneTimeTearDown()
         {
-            driver.Close();
-            driver.Quit();
+            //driver.Close();
+           // driver.Quit();
         }
 
     }
