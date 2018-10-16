a50c89e45e0a1243f9f92778b423562ca192afde
LuizaMytsyk <luiza.mytsyk@gmail.com>
2018-10-16 10:50:51 +0300

    init commit

---
+++ DelitteLib/LoginPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace DeloitteLib
+{
+    public class LoginPage : BaseClass
+    {
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+        }
+        public void SingIn(string userName, string password)
+        {
+            _username.SendKeys(userName);
+            _password.SendKeys(password);
+            _submitButton.Click();
+        }
+
+        [FindsBy(How = How.Id, Using = "username")]
+        private IWebElement _username;
+
+        [FindsBy(How = How.XPath, Using = "//input[@placeholder='Password']")]
+        private IWebElement _password;
+
+        [FindsBy(How = How.Id, Using = "login-submit")]
+        private IWebElement _submitButton;
+    }
+}
---
+++ Deloitte/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DeloitteLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DeloitteLib")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6de49fb9-219b-4760-9c27-43f97211593b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
