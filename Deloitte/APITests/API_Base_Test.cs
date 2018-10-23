4781ae529b91a92094331b40a5e6b8e4592ab1a0
LuizaMytsyk <luiza.mytsyk@gmail.com>
2018-10-23 12:14:33 +0300

    added APIBaseTest

---
+++ Deloitte/APITests/API_Base_Test.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+using DeloitteLib;
+using DeloitteTests;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using RestSharp;
+using System.Collections.Generic;
+
+namespace Deloitte
+{
+    [TestFixture]
+    public class API_Base_Test
+    {
+        public string sessionId;
+
+        [OneTimeSetUp]
+        public void Login()
+        {
+            RestClient restClient = new RestClient("https://perf.exalinkservices.com:8443/apigateway/v1/sessions");
+            RestRequest restRequest = new RestRequest(Method.POST);
+            restRequest.AddHeader("Content-type", "application/json");
+            restRequest.AddJsonBody(
+                new
+                {
+                    username = "gp_integrator",
+                    password = "Dummy#123"
+                });
+
+            IRestResponse responce = restClient.Execute(restRequest);
+
+            RestSharp.Deserializers.JsonDeserializer deserial = new RestSharp.Deserializers.JsonDeserializer();
+            var JSONObj = deserial.Deserialize<Dictionary<string, string>>(responce);
+            sessionId = JSONObj["sessionId"];
+
+            Assert.Warn(sessionId);
+
+        }
+
+    }
+}
---
+++ Deloitte/APITests/Test_API_ProjectCreated.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using DeloitteLib;
+using DeloitteTests;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using RestSharp;
+using System.Collections.Generic;
+
+namespace Deloitte
+{
+    [TestFixture]
+    public class Test_API_ProjectCreated : API_Base_Test
+    {
+
+
+        [Test]
+        public void CreateProject()
+        {
+            RestClient restClient = new RestClient("https://perf.exalinkservices.com:8443/gpproj/v1/projects/");
+            RestRequest restRequest = new RestRequest(Method.POST);
+            restRequest.AddHeader("Content-type", "application/json");
+            restRequest.AddHeader("x-client", "umbrella");
+            restRequest.AddHeader("Authorization", "SessionID " + sessionId);
+
+            restRequest.AddJsonBody(
+                new
+                {
+                    due_date = "2018-10-26",
+                    end_month = "12",
+                    end_year = "2018",
+                    name = "qwertdy96f66555827",
+                    start_month = "05",
+                    start_year = "2018",
+                    type = "Adhoc"
+                });
+
+            IRestResponse responce = restClient.Execute(restRequest);
+
+            RestSharp.Deserializers.JsonDeserializer deserial = new RestSharp.Deserializers.JsonDeserializer();
+            var JSONObj = deserial.Deserialize<Dictionary<string, string>>(responce);
+            string status = JSONObj["status"];
+
+            Assert.AreEqual("success", status);
+        }
+
+    }
+}
