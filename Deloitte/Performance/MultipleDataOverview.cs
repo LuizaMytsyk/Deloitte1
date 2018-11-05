77d90337645bd5919f14c9a4c9fe2986081ba5bf
ManuilovaTA <40334678+ManuilovaTA@users.noreply.github.com>
2018-11-05 16:43:21 +0200

    Data overview test

--- Deloitte/Performance/MultipleDataOverview.cs
+++ Deloitte/Performance/MultipleDataOverview.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using NUnit.Framework;
-using DeloitteLib;
-using DeloitteTests;
-using OpenQA.Selenium.Support.UI;
-using OpenQA.Selenium;
 using System.Threading.Tasks;
 using RestSharp;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 namespace Deloitte
 {
@@ -17,6 +14,24 @@ namespace Deloitte
         [Test]
         public void TestMethod1()
         {
+            Stopwatch stopWatch = new Stopwatch();
+            Console.WriteLine("Start");
+            stopWatch.Start();
+            Parallel.For(0, 14, i => DataOverView(i));
+            stopWatch.Stop();
+            Console.WriteLine("Runtime " + stopWatch.Elapsed);
+        }
+
+        async Task DataOverView(int i)
+        {
+            RestClient restClient = new RestClient(baseUrl + "/gptransactions/v1/overview");
+            RestRequest restRequest = new RestRequest(Method.GET);
+            restRequest.AddHeader("Content-type", "application/json");
+            restRequest.AddHeader("x-client", "umbrella");
+            restRequest.AddHeader("Authorization", "SessionID " + sessionId);
+            Console.WriteLine("Thread is run " + i);
+
+            IRestResponse responce = restClient.Execute(restRequest);
         }
     }
 }
